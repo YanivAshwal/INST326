95ad40b3cca3d5d5b964cd4955680012831102d9
Yaniv Ashwal <yaniv.a@circ.zone>
2025-03-31 09:43:06 -0400

    Added descriptor and functionality

--- twoTone.py
+++ twoTone.py
@@ -1,2 +1,12 @@
 def tones():
-    print(2)
+    print("How many Tones you may ask?")
+    print("...")
+    print("drumroll please...")
+    print("*Drumroll*")
+    print("\n\n\n")
+
+    print("\t2")
+    print("\t2 tones.")
+
+
+tones()
